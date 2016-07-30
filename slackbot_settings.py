e825e62cc0eba4da46f4ac2664583d1d3b1c09ff
Justin Snyder <pylogix@outlook.com>
2016-07-30 15:29:10 -0400

    completed settings for the bot

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+import os
+
+API_TOKEN = os.environ.get('SLACK_API_TOKEN', '')
+
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+
+PLUGINS = [
+    'slackbot.plugins',
+    'punchy.plugins',
+]
